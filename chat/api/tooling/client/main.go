9309c5d118d4543e7fb145ff61638b4c42944f16
William Kennedy <bill@ardanlabs.com>
2025-02-06 12:52:24 -0500

    first handshake try

--- chat/api/tooling/client/main.go
+++ chat/api/tooling/client/main.go
@@ -1,5 +1,70 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	if err := hack1(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func hack1() error {
+	const url = "http://localhost:3000/connect"
+	req := make(http.Header)
+
+	socket, _, err := websocket.DefaultDialer.Dial(url, req)
+	if err != nil {
+		return fmt.Errorf("dial: %w", err)
+	}
+
+	defer socket.Close()
+
+	// -------------------------------------------------------------------------
+
+	_, msg, err := socket.ReadMessage()
+	if err != nil {
+		return fmt.Errorf("read: %w", err)
+	}
+
+	if string(msg) != "HELLO" {
+		return fmt.Errorf("unexpected message: %s", msg)
+	}
+
+	// -------------------------------------------------------------------------
+
+	user := struct {
+		ID   uuid.UUID
+		Name string
+	}{
+		ID:   uuid.New(),
+		Name: "Bill Kennedy",
+	}
+
+	data, err := json.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("marshal: %w", err)
+	}
+
+	if err := socket.WriteMessage(websocket.TextMessage, data); err != nil {
+		return fmt.Errorf("write: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+
+	_, msg, err = socket.ReadMessage()
+	if err != nil {
+		return fmt.Errorf("read: %w", err)
+	}
+
+	fmt.Println(string(msg))
+
+	return nil
 }
