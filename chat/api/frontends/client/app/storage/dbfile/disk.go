994d3430c8070cd029976fa870f926d5e698cfcf
William Kennedy <bill@ardanlabs.com>
2025-03-21 12:00:28 -0500

    code cleanup

---
+++ chat/api/frontends/client/app/storage/dbfile/disk.go
@@ -0,0 +1,178 @@
+package dbfile
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+const (
+	dbDirName     = "db"
+	dbMsgsDirName = "msgs"
+	dbFileName    = "data.json"
+)
+
+var (
+	dbFileDir string
+	dbMsgsDir string
+	dbFile    string
+)
+
+type dataFileUser struct {
+	ID   common.Address `json:"id"`
+	Name string         `json:"name"`
+}
+
+type dataFile struct {
+	MyAccount dataFileUser   `json:"my_account"`
+	Contacts  []dataFileUser `json:"contacts"`
+}
+
+func newDB(filePath string, myAccountID common.Address) (dataFile, error) {
+	dbFileDir = filepath.Join(filePath, dbDirName)
+	dbMsgsDir = filepath.Join(filePath, dbDirName, dbMsgsDirName)
+	dbFile = filepath.Join(filePath, dbDirName, dbFileName)
+
+	os.MkdirAll(dbFileDir, os.ModePerm)
+	os.MkdirAll(dbMsgsDir, os.ModePerm)
+
+	var df dataFile
+
+	_, err := os.Stat(dbFile)
+	switch {
+	case err != nil:
+		df, err = createDBOnDisk(myAccountID)
+
+	default:
+		df, err = readDBFromDisk()
+		if df.MyAccount.ID != myAccountID {
+			return dataFile{}, fmt.Errorf("id mismatch: got: %s exp: %s", df.MyAccount.ID.Hex(), myAccountID.Hex())
+		}
+	}
+
+	if err != nil {
+		return dataFile{}, fmt.Errorf("config: %w", err)
+	}
+
+	return df, nil
+}
+
+func createDBOnDisk(myAccountID common.Address) (dataFile, error) {
+	f, err := os.Create(dbFile)
+	if err != nil {
+		return dataFile{}, fmt.Errorf("config data file create: %w", err)
+	}
+	defer f.Close()
+
+	df := dataFile{
+		MyAccount: dataFileUser{
+			ID:   myAccountID,
+			Name: "Anonymous",
+		},
+		Contacts: []dataFileUser{
+			{
+				ID:   common.Address{},
+				Name: "Sample Contact",
+			},
+		},
+	}
+
+	jsonDoc, err := json.MarshalIndent(df, "", "    ")
+	if err != nil {
+		return dataFile{}, fmt.Errorf("config data file marshal: %w", err)
+	}
+
+	if _, err := f.Write(jsonDoc); err != nil {
+		return dataFile{}, fmt.Errorf("config data file write: %w", err)
+	}
+
+	return df, nil
+}
+
+func readDBFromDisk() (dataFile, error) {
+	f, err := os.Open(dbFile)
+	if err != nil {
+		return dataFile{}, fmt.Errorf("id data file open: %w", err)
+	}
+	defer f.Close()
+
+	var df dataFile
+	if err := json.NewDecoder(f).Decode(&df); err != nil {
+		return dataFile{}, fmt.Errorf("config decode: %w", err)
+	}
+
+	return df, nil
+}
+
+func flushDBToDisk(df dataFile) error {
+	f, err := os.Create(dbFile)
+	if err != nil {
+		return fmt.Errorf("config data file create: %w", err)
+	}
+	defer f.Close()
+
+	jsonDF, err := json.MarshalIndent(df, "", "    ")
+	if err != nil {
+		return fmt.Errorf("config data file marshal: %w", err)
+	}
+
+	if _, err := f.Write(jsonDF); err != nil {
+		return fmt.Errorf("config data file write: %w", err)
+	}
+
+	return nil
+}
+
+func readMsgsFromDisk(id common.Address) ([]string, error) {
+	fileName := filepath.Join(dbMsgsDir, id.Hex()+".msg")
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		return []string{}, nil
+	}
+	defer f.Close()
+
+	var msgs []string
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		s := scanner.Text()
+		msgs = append(msgs, s)
+	}
+
+	return msgs, nil
+}
+
+func flushMsgToDisk(id common.Address, msg string) error {
+	fileName := filepath.Join(dbMsgsDir, id.Hex()+".msg")
+
+	var f *os.File
+
+	_, err := os.Stat(fileName)
+	switch {
+	case err != nil:
+
+		f, err = os.Create(fileName)
+		if err != nil {
+			return fmt.Errorf("message file create: %w", err)
+		}
+
+	default:
+		f, err = os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			return fmt.Errorf("message file open: %w", err)
+		}
+	}
+
+	defer f.Close()
+
+	if _, err := f.WriteString(msg + "\n"); err != nil {
+		return fmt.Errorf("message file write: %w", err)
+	}
+
+	return nil
+}
